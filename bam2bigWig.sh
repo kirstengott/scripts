bc9f0b150ba76dc1b0f2b7844756f930a28f9d8f
kirstengott <k.gotting@gmail.com>
2016-06-21 13:29:26 -0500

    added stranded/unstranded option and a check for BED file so it will not be remade

--- bam2bigWig.sh
+++ bam2bigWig.sh
@@ -15,7 +15,7 @@ usage="Usage: ./bam2bigWig -b <input_bamfile> -d <genome_database> -c <genome_ch
                 -c: location of chromosome lengths file. [REQUIRED]"
 
 
-while getopts ":hb:d:c:" opt; do
+while getopts ":hb:d:c:s" opt; do
     case $opt in
 	h)
 	    echo "$usage" >&2
@@ -33,6 +33,10 @@ while getopts ":hb:d:c:" opt; do
 	    chromosome_file="$OPTARG" >&2
 	    echo "Chromosome File: $OPTARG" >&2
 	    ;;
+        s)
+            strand='TRUE'
+            echo "Creating biwWigs for both strands" >&2
+            ;;
 	\?)
 	    echo "Invalid option: -$OPTARG" >&2
 	    echo "$usage" >&2
@@ -58,29 +62,41 @@ output_bed=${bam_base}.bed
 
 echo 'Making BED file'
 
-bamToBed -i ${input_bam} -split > ${output_bed}
-
-
-echo 'Making plus bedgraph'
-
-${awk} '{if($6=="+") print}' ${output_bed} | ${sort} -k1,1 | ${bedItemOverlapCount} ${genome_name} -chromSize=${chromosome_file} stdin | ${sort} -k1,1 -k2,2n >${bam_base}.plus.bedGraph
-
-
-echo "Making plus bigwig"
-
-${bedGraphToBigWig} ${bam_base}.plus.bedGraph ${chromosome_file} ${bam_base}.plus.bw
+if [[ -e ${output_bed} ]]; then
+    echo 'BED file exists, moving to make bedgraph'
+else
+    bamToBed -i ${input_bam} -split > ${output_bed}
+fi
 
 
 
-echo 'Making minus bedgraph'
+if [[ $strand == "TRUE" ]]; then
 
-${awk} '{if($6=="-") print}' ${output_bed} | ${sort} -k1,1 | ${bedItemOverlapCount} ${genome_name} -chromSize=${chromosome_file} stdin | ${sort} -k1,1 -k2,2n | ${awk} '{OFS="\t"; print $1,$2,$3,"-"$4}' >${bam_base}.minus.bedGraph
-
-echo 'Making minus bigwid'
-
-${bedGraphToBigWig} ${bam_base}.minus.bedGraph ${chromosome_file} ${bam_base}.minus.bw
+    echo 'Making plus bedgraph'
+    ${awk} '{if($6=="+") print}' ${output_bed} | ${sort} -k1,1 | ${bedItemOverlapCount} ${genome_name} -chromSize=${chromosome_file} stdin | ${sort} -k1,1 -k2,2n >${bam_base}.plus.bedGraph
 
 
+    echo "Making plus bigwig"
+    ${bedGraphToBigWig} ${bam_base}.plus.bedGraph ${chromosome_file} ${bam_base}.plus.bw
+
+
+    echo 'Making minus bedgraph'
+    ${awk} '{if($6=="-") print}' ${output_bed} | ${sort} -k1,1 | ${bedItemOverlapCount} ${genome_name} -chromSize=${chromosome_file} stdin | ${sort} -k1,1 -k2,2n | ${awk} '{OFS="\t"; print $1,$2,$3,"-"$4}' >${bam_base}.minus.bedGraph
+
+    echo 'Making minus bigwig'
+    ${bedGraphToBigWig} ${bam_base}.minus.bedGraph ${chromosome_file} ${bam_base}.minus.bw
+
+else
+    echo 'Making bedgraph'
+
+    cat ${output_bed} | ${sort} -k1,1 | ${bedItemOverlapCount} ${genome_name} -chromSize=${chromosome_file} stdin | ${sort} -k1,1 -k2,2n | ${awk} '{OFS="\t"; print $1,$2,$3,"-"$4}' >${bam_base}.bedGraph
+
+    echo 'Making bigwig'
+
+    ${bedGraphToBigWig} ${bam_base}.bedGraph ${chromosome_file} ${bam_base}.bw
+
+fi
+    
 
 #cat ${output_bed} | ${sort} -k1,1 | ${bedItemOverlapCount} ${genome_name} -chromSize=${chromosome_file} stdin | ${sort} -k1,1 -k2,2n >${bam_base}.bedGraph
 
