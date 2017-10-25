58a5e716118a5dda0c76056a91fb8abda88e9572
Kirsten Leigh Gotting <k.gotting@gmail.com>
2017-10-25 11:10:10 -0500

    fixing indent

--- parseReciprocalBlast.R
+++ parseReciprocalBlast.R
@@ -18,24 +18,47 @@ reference <- data.table::fread(args[1], data.table = FALSE)
 invert    <- data.table::fread(args[2], data.table = FALSE)
 
 
+cols <- c("query", 
+          "subject", 
+          "perc_id", 
+          "align_len", 
+          "mismatches", 
+          "gaps", 
+          "q_start", 
+          "q_end", 
+          "s_start", 
+          "s_end", 
+          "e_val", 
+          "bit_score")
 
-colnames(reference) <- paste0('reference_', c("query", "subject", "perc_id", "align_len", "mismatches", "gaps", "q_start", "q_end", "s_start", "s_end", "e_val", "bit_score"))
-colnames(invert)    <- paste0('invert_', c("query", "subject", "perc_id", "align_len", "mismatches", "gaps", "q_start", "q_end", "s_start", "s_end", "e_val", "bit_score"))
+colnames(reference) <- paste0('reference_', cols)
+colnames(invert)    <- paste0('invert_', cols)
 
 
 
+referenceRank <- reference %>% 
+  filter(reference_e_val <= 1e-3) %>% 
+  group_by(reference_query) %>% 
+  distinct(reference_query, reference_subject) %>%
+  mutate(rank1 = floor(rank(-reference_bit_score))) %>%
+  ungroup() %>% filter(rank1 == 1) %>% 
+  rename('common_subject'= reference_subject)
 
-referenceRank <- reference %>% filter(reference_e_val <= 1e-3) %>% group_by(reference_query) %>% distinct(reference_query, reference_subject) %>%
-    mutate(rank1 = floor(rank(-reference_bit_score))) %>%
-        ungroup() %>% filter(rank1 == 1) %>% rename('common_subject'= reference_subject)
-
-invertRank <- invert %>% filter(invert_e_val <= 1e-3) %>% group_by(invert_query) %>% distinct(invert_query, invert_subject) %>%
-    mutate(rank2 = floor(rank(-invert_bit_score))) %>%
-        ungroup()%>% rename('common_subject' = invert_query, 'reference_query' = invert_subject)
+invertRank <- invert %>% 
+  filter(invert_e_val <= 1e-3) %>% 
+  group_by(invert_query) %>% 
+  distinct(invert_query, invert_subject) %>%
+  mutate(rank2 = floor(rank(-invert_bit_score))) %>%
+  ungroup()%>% 
+  rename('common_subject' = invert_query, 
+         'reference_query' = invert_subject)
 
 
 ranker    <- left_join(referenceRank, invertRank)
-rankfinal <- ranker %>% select(reference_query, common_subject, reference_e_val, rank2)
+rankfinal <- ranker %>% select(reference_query, 
+                               common_subject, 
+                               reference_e_val, 
+                               rank2)
 
 
 
