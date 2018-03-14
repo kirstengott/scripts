c7619884f50188f10fea5ce60c0b5881dbbb2b16
Kirsten Gotting <k.gotting@gmail.com>
2018-03-14 10:51:44 -0500

    adding a script to pull down SRA files, and the first version of my ggplot theme

---
+++ get_sra.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+usage="Usage: ./get_sra.sh -f <SraRunInfo.csv>
+                -h: this help message.
+                -f: SraRunInfo.csv [REQUIRED]"
+
+
+while getopts ":hf:" opt; do
+    case $opt in
+	h)
+	    echo "$usage" >&2
+	    exit 1
+	    ;;
+	f)
+	    file="$OPTARG" >&2
+	    echo "Bam File: $OPTARG" >&2
+	    ;;
+	\?)
+	    echo "Invalid option: -$OPTARG" >&2
+	    echo "$usage" >&2
+	    exit 1
+	    ;;
+	:)
+	    echo "Option -$OPTARG requires an argument." >&2
+	    echo "$usage" >&2
+	    exit 1
+	    ;;
+    esac
+done
+
+ids=`cut -d , -f 1 $file | grep -v Run`
+
+for id in $ids
+do
+    prefetch $id
+    fastq-dump --split-3 --gzip $id
+done
---
+++ theme_kirsten.R
@@ -0,0 +1,13 @@
+
+theme_kirsten <- function(rot = TRUE, presentation = FALSE, angle = 45, text_size = 12){
+  my_theme <- theme(strip.text.x = element_text(face="bold"),
+                    strip.text.y = element_text(face="bold"),
+                    strip.background = element_rect(fill = 'seashell'))
+  if(rot == TRUE){
+    my_theme <- my_theme + theme(axis.text.x = element_text(angle = angle, hjust = 1))
+  }
+  if(presentation == TRUE){
+    my_theme <- my_theme + theme(text = element_text(size = 24))
+  }
+  theme_bw(base_size = text_size) + my_theme
+}
