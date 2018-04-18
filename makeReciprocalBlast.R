b0299757dc2a85ff4269d0edaab5b28fd395a60d
Kirsten Gotting <k.gotting@gmail.com>
2018-04-18 11:09:33 -0500

    blast parser non-redundant, updating personal ggplot theme

--- makeReciprocalBlast.R
+++ makeReciprocalBlast.R
@@ -1,16 +1,19 @@
 #!/usr/bin/env Rscript
+
 args <- commandArgs(TRUE)
 if (length(args) == 0L || any(c('-h', '--help') %in% args)) {
-  message('usage: path/to/makeReciprocalBlast.R blastdbs outdir
-          blastdbs          path to the blastdbs
-          outdir            path to output
-          blasttype         type of blast
+  message('usage: path/to/makeReciprocalBlast.R blastdbs outdir blasttype extension num_threads
+          blastdbs          path to the blastdb directory
+          outdir            path to output directory
+          blasttype         type of blast (blastp or blastn)
+          extension         fasta extension (fa or fasta)
+	  num_threads       cores to use
           -h, --help        to print help messages')
   q('no')
 }
 library(tools)
 
-#args <- c('../data/300_genomes_proteins/', 'recips', 'blastp')
+args <- c('db', 'blastn' ,'blastn', 'fasta')
 
 blast_type <- args[3]
 
@@ -19,7 +22,7 @@ make_blast <- function(x, y){
   y_mod <- file_path_sans_ext(basename(y))
   out   <- paste0(args[2], "/", x_mod, "_", y_mod, ".", blast_type)
   paste(blast_type, '-query', x, '-db', y,
-  '-max_target_seqs 1 -evalue 0.001 -num_threads 6 -outfmt 6 -out', out, sep = " ")
+  '-max_target_seqs 1 -evalue 0.001 -num_threads', args[5], '-outfmt 6 -out', out, sep = " ")
 }
 
 make_parse <- function(x, y){
@@ -31,7 +34,11 @@ make_parse <- function(x, y){
   paste('parseReciprocalBlast.R', blast_reference, blast_invert, out, sep = " ")
 }
 
-fas <- list.files(args[1], full.names = TRUE, pattern = 'fas$')
+
+
+fas <- list.files(args[1], full.names = TRUE, pattern = paste0(args[4], '$'))
+
+unique_pairs <- combn(fas, 2, simplify = FALSE)
 
 blast_commands <- unlist(
   lapply(fas, function(x){
@@ -39,22 +46,13 @@ blast_commands <- unlist(
   make_blast(x, fa_list)
 }))
 
-parse_commands <- unlist(
-  lapply(fas, function(x){
-    fa_list <- fas[which(!fas == x)]
-    make_parse(x, fa_list)
+blast_parse_commands <- unlist(
+  lapply(unique_pairs, function(x){
+    make_parse(x[1], x[2])
 }))
 
 
 
-
-blast_parse_commands <- unlist(
-  lapply(fas, function(x){
-    fa_list <- fas[which(!fas == x)]
-    make_parse(x, fa_list)
-  }))
-
-
-write(x = blast_commands, file = 'reciprocal_blasts.txt')
-write(x = blast_parse_commands, file = 'parse_reciprocal_blasts.txt')
+write(x = blast_commands, file = 'reciprocal_blasts.sh')
+write(x = blast_parse_commands, file = 'parse_reciprocal_blasts.sh')
 
--- theme_kirsten.R
+++ theme_kirsten.R
@@ -2,7 +2,7 @@
 theme_kirsten <- function(rot = TRUE, presentation = FALSE, angle = 45, text_size = 12){
   my_theme <- theme(strip.text.x = element_text(face="bold"),
                     strip.text.y = element_text(face="bold"),
-                    strip.background = element_rect(fill = 'seashell'))
+                    strip.background = element_rect(fill = 'grey96'))
   if(rot == TRUE){
     my_theme <- my_theme + theme(axis.text.x = element_text(angle = angle, hjust = 1))
   }
