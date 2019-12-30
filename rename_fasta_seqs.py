27659ddeea110324964623011495dcab8d29521d
Kirsten Gotting <k.gotting@gmail.com>
2019-12-30 14:00:02 -0600

    updating

--- rename_fasta_seqs.py
+++ rename_fasta_seqs.py
@@ -5,21 +5,21 @@ import sys
 import os
 
 
-def main(argv):
-    if len(argv) != 3:
-        sys.stderr.write("Usage: %s <fastafile> <fastaoutfile> <id_map_out>\n" % os.path.basename(sys.argv[0]))
-        sys.exit(1)
-    fasta = sys.argv[1]
 
+
+def rename_fasta_seqs(fasta):
     fh = pysam.FastxFile(fasta)
-
-
-
-    count=0
-
     fa_out     = open(sys.argv[2], 'w')
-    id_map_out = open(sys.argv[3], 'w')
-
+    fasta_base = os.path.basename(fasta)
+    fa_out_dir = os.path.dirname(sys.argv[2])
+    id_map     = fasta_base.split(".")[:-1]
+    if len(id_map) > 1:
+        id_map = ".".join(id_map)
+    else:
+        id_map = id_map[0]
+    id_map = fa_out_dir + "/" + id_map + "_id_map.txt"
+    id_map_out = open(id_map, 'w')
+    count=0
     for entry in fh:
         seq            = entry.sequence
         new_id = 'seq' + str(count)
@@ -28,10 +28,19 @@ def main(argv):
         fa_out.write(out_seq)
         id_map_out.write(out_id_map)
         count += 1
-
-    fa_out.close()
     id_map_out.close()
+    fa_out.close()
     fh.close()
+
+    
+
+def main(argv):
+    if len(argv) != 2:
+        sys.stderr.write("Usage: %s <fastafile> <fastaoutfile>\n" % os.path.basename(sys.argv[0]))
+        sys.exit(1)
+    fasta = sys.argv[1]
+    rename_fasta_seqs(fasta)
+
 
 
     
